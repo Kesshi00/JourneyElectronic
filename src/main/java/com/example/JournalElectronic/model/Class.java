fb8a7e2c19589024c6898f9c94aebafe292da119
draxolini <draxolini@interia.pl>
2020-02-04 21:31:17 +0100

    Changes in logged.html and LoggedController.java

--- src/main/java/com/example/JournalElectronic/model/Class.java
+++ src/main/java/com/example/JournalElectronic/model/Class.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Class {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int classNr;
     private int roomNr;
